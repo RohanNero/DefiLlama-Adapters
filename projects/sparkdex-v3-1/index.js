1fca98029a30623d3ed3c9c15b83776dd42f52d8
rohannero <rohanzarathustra@gmail.com>
2026-02-05 13:49:45 -0500

    blacklist SPRK

--- projects/sparkdex-v3-1/index.js
+++ projects/sparkdex-v3-1/index.js
@@ -1,6 +1,9 @@
 const { uniV3Export } = require("../helper/uniswapV3");
 
 module.exports = uniV3Export({
-  flare: { factory: '0x8A2578d23d4C532cC9A98FaD91C0523f5efDE652', fromBlock: 
-    30717263, },
+  flare: {
+    factory: '0x8A2578d23d4C532cC9A98FaD91C0523f5efDE652',
+    fromBlock: 30717263,
+    blacklistedTokens: ['0x657097cc15fdec9e383db8628b57ea4a763f2ba0'] // SPRK
+  },
 })
