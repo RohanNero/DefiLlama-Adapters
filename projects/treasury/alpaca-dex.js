ff0599993f1a1a0817bcc06146b24ed395b591a6
rohannero <rohanzarathustra@gmail.com>
2026-02-16 13:49:29 -0500

    wip: alpaca dex treasury

---
+++ projects/treasury/alpaca-dex.js
@@ -0,0 +1,8 @@
+const { treasuryExports } = require("../helper/treasury");
+
+module.exports = treasuryExports({
+    keeta: {
+        owners: ['keeta_aabfxcosaprdm5v24g4nkc7uag5k5kfkkgkior6wqsb7dig43t4kd5ukwi7iwfi'],
+        ownTokens: ['keeta_ao55q4okjv4hrbo7z7zl3hivrf64og3fpokup5hvt2wfejim5mxzxcykboc3w'],
+    },
+})
