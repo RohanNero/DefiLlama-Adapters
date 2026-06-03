bd97e87e45a197d2f6b57848b8035282d18f2f51
rohannero <rohanzarathustra@gmail.com>
2026-06-03 14:45:23 -0400

    wip: nemo rewrite

--- projects/nemo/index.js
+++ projects/nemo/index.js
@@ -1,12 +1,5 @@
-const ADDRESSES = require('../helper/coreAssets.json')
 const sui = require("../helper/chain/sui");
 const BigNumber = require("bignumber.js");
-const {COIN_CONFIG} = require("./coinConfig.js");
-
-const {fromU64: desU64, fromU128: desU128} = require("../helper/chain/sui");
-const {getExchangeRate} = require("./price");
-const {MMT_TYPE_CONFIG} = require("./coinConfig");
-const {getVaultTvlByAmountB, getDynamicFieldObject} = require("./util");
 
 const nemoPackageId = "0x2b71664477755b90f9fb71c9c944d5d0d3832fec969260e3f18efc7d855f57c4";
 const syTableParentId = "0xcb74e46f4049e1afc3edcd77172ac6f9adfe1068cec6ca99ff6b80ca7879bd33";
@@ -31,100 +24,35 @@ const watchCoinType = [
   '0x285b49635f4ed253967a2a4a5f0c5aea2cbd9dd0fc427b4086f3fad7ccef2c29::i_sui::I_SUI',
   '0x549e8b69270defbfafd4f94e17ec44cdbdd99820b33bda2278dea3b9a32d3f55::cert::CERT',
   '0xd01d27939064d79e4ae1179cd11cfeeff23943f32b1a842ea1a1e15a0045d77d::st_sbuck::ST_SBUCK',
-  "0x7f29e761222a44b2141596e920edcc9049f8610f9d33f5354454d088e1f53b62::x_sui_sui_nevlp::X_SUI_SUI_NEVLP",
-  "0x295d7f569467934c934e6101284628ecbbb1e68d5c5baa8d4667ff09c42068ad::suiusdt_usdc_nevlp::SUIUSDT_USDC_NEVLP"
-];
-
-const watchCoinTypeNotConvert = [
-  '0xf325ce1300e8dac124071d3152c5c5ee6174914f8bc2161e88329cf579246efc::afsui::AFSUI',
-  '0x790f258062909e3a0ffc78b3c53ac2f62d7084c3bab95644bdeb05add7250001::super_sui::SUPER_SUI',
-  '0xd1b72982e40348d069bb1ff701e634c117bb5f741f44dff91e472d3b01461e55::stsui::STSUI',
-  '0x83556891f4a0f233ce7b05cfe7f957d4020492a34f5405b2cb9377d060bef4bf::spring_sui::SPRING_SUI',
-  '0x02358129a7d66f943786a10b518fdc79145f1fc8d23420d9948c4aeea190f603::fud_sui::FUD_SUI',
-  '0x549e8b69270defbfafd4f94e17ec44cdbdd99820b33bda2278dea3b9a32d3f55::cert::CERT',
+  '0x7f29e761222a44b2141596e920edcc9049f8610f9d33f5354454d088e1f53b62::x_sui_sui_nevlp::X_SUI_SUI_NEVLP',
+  '0x295d7f569467934c934e6101284628ecbbb1e68d5c5baa8d4667ff09c42068ad::suiusdt_usdc_nevlp::SUIUSDT_USDC_NEVLP',
 ];
 
 async function tvl(api) {
   const marketIds = await sui.queryEvents({
     eventType: `${nemoPackageId}::market_factory::MarketCreatedEvent`,
-    transform: i => i.market_id
+    transform: i => i.market_id,
   });
-
   const markets = await sui.getObjects(marketIds);
 
   for (const market of markets) {
     if (!market) continue;
+    const { type, fields } = market;
 
-    const {type, fields} = market;
-    await getTvl(type, fields, api);
-  }
-}
+    const typeString = type.replace(">", "").split("<")[1];
+    if (!typeString) continue;
 
-async function getTvl(type, fields, api) {
-  const typeString = type.replace(">", "").split("<")[1];
-  if (!typeString) return null;
+    const syTokens = typeString.split(", ")[0].slice(2);
+    const tokensObject = await sui.getDynamicFieldObject(
+      syTableParentId, syTokens, { idType: "0x1::type_name::TypeName" }
+    );
+    if (!tokensObject) continue;
 
-  const syTokens = typeString.split(", ")[0].slice(2);
+    const syCoinType = "0x" + tokensObject.fields.value.fields.name;
+    if (!watchCoinType.includes(syCoinType)) continue;
 
-  const tokensObject = await getDynamicFieldObject(
-    syTableParentId, syTokens, {idType: "0x1::type_name::TypeName"});
-  if (!tokensObject || !tokensObject.content) return null;
-  const tokens = "0x" + tokensObject.content.fields.value.fields.name;
-
-  const coinConfig = COIN_CONFIG[tokens];
-
-  if (!coinConfig) return null;
-  if (!watchCoinType.includes(coinConfig.coinType)) return null;
-
-  const txBlockBytes = await getExchangeRate(coinConfig);
-
-  const inspectionResult = await sui.call(
-    'sui_devInspectTransactionBlock',
-    ['0x0000000000000000000000000000000000000000000000000000000000000000',
-      Buffer.from(txBlockBytes).toString('base64')],
-    {withMetadata: true}
-  );
-
-  if (inspectionResult?.effects?.status?.status !== 'success') {
-    return null;
-  }
-
-  const returnValues = inspectionResult.results[inspectionResult.results?.length - 1].returnValues;
-  const res1 = returnValues[0][0];
-  const res2 = returnValues[1][0];
-
-  if (coinConfig.provider === 'Nemo') {
-    const priceVoucher1 = desU64(Uint8Array.from(res1));
-    const priceVoucher2 = desU64(Uint8Array.from(res2));
-    const pt2SyAmount = new BigNumber(fields.total_pt).div(priceVoucher1).times(priceVoucher2);
-    let syBalance = BigNumber.sum(pt2SyAmount, new BigNumber(fields.total_sy));
-
-    const vault = await sui.getObject(MMT_TYPE_CONFIG[coinConfig.coinType].VAULT_ID);
-    const amountB = await getVaultTvlByAmountB(vault);
-
-    const totalSupply = vault.fields.treasury_cap.fields.total_supply.fields.value;
-
-    const lpTokenPrice = BigNumber(amountB).div(BigNumber(totalSupply));
-
-    api.add(coinConfig.underlyingCoinType, syBalance.times(lpTokenPrice).toFixed(0));
-  } else {
-    const priceVoucher1 = desU128(Uint8Array.from(res1));
-    const priceVoucher2 = desU128(Uint8Array.from(res2));
-
-    let rate1 = new BigNumber(priceVoucher1).div(new BigNumber(2).pow(64)).toString();
-    let rate2 = new BigNumber(priceVoucher2).div(new BigNumber(2).pow(64)).toString();
-
-    const pt2SyAmount = new BigNumber(fields.total_pt).div(rate1);
-    let syBalance = BigNumber.sum(pt2SyAmount, new BigNumber(fields.total_sy));
-
-    if (watchCoinTypeNotConvert.includes(coinConfig.coinType)) {
-      // console.log(`coinType: ${coinConfig.coinType}, syBalance: ${syBalance.toNumber()}, marketId: ${fields.id.id}`);
-      api.add(tokens, syBalance.toNumber());
-    } else {
-      let underlyingBalance = syBalance.multipliedBy(rate2);
-      // console.log(`coinType: ${coinConfig.coinType}, syBalance: ${underlyingBalance.toNumber()}, marketId: ${fields.id.id}, rate1: ${rate1}, rate2: ${rate2}`);
-      api.add(coinConfig.underlyingCoinType, underlyingBalance.toNumber());
-    }
+    // wip: missing PT
+    api.add(syCoinType, fields.total_sy);
   }
 }
 
