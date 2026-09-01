674e7a05fa09f7b150d089b69460f14d40ad89f2
rohannero <rohanzarathustra@gmail.com>
2026-09-01 15:50:06 -0400

    add umbra privacy treasury

--- registries/treasury.js
+++ registries/treasury.js
@@ -3,6 +3,7 @@ const ADDRESSES = require('../projects/helper/coreAssets.json')
 const { nullAddress } = require('../projects/helper/treasury')
 const { nullAddress: tokenMappingNullAddress } = require('../projects/helper/tokenMapping')
 const bitcoinAddressBook = require('../projects/helper/bitcoin-book/index.js')
+const solana = require('../projects/helper/solana.js')
 
 // Treasury registry.
 //
@@ -5931,6 +5932,12 @@ const configs = {
     sui: {
       owners: ['0xb9a09efd534d29cc9f990db26b2dab00289f32de0cdcefa68c6808de208bc9cb'],
       ownTokens: ['0xf82dc05634970553615eef6112a1ac4fb7bf10272bf6cbe0f80ef44a6c489385::typus::TYPUS'],
+    },
+  },
+  'treasury/umbra-privacy': {
+    solana: {
+      owners: ['6VsC8PuKkXm5xo54c2vbrAaSfQipkpGHqNuKTxXFySx6'],
+      ownTokens: ['PRVT6TB7uss3FrUd2D9xs2zqDBsa3GbMJMwCQsgmeta'], // UMBRA
     },
   },
   'treasury/uniswap': {
